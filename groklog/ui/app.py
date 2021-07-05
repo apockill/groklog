cf3094946e0784bbe194c8bf3bdb00ec3682b1c3
Alex Thiel <apocthiel@gmail.com>
2021-07-05 16:18:26 -0700

    Update main scene with a preliminary layout

--- groklog/ui/app.py
+++ groklog/ui/app.py
@@ -1,24 +1,42 @@
-from asciimatics import widgets
+from asciimatics import effects, renderers, widgets
+from asciimatics.event import KeyboardEvent
+from asciimatics.exceptions import StopApplication
+from asciimatics.screen import Screen
 from asciimatics.widgets import Frame, Layout
 
-from groklog.shell import Shell
+from groklog.process_node import ShellProcessIO
 from groklog.ui.terminal import Terminal
 
 
 class GrokLog(Frame):
-    def __init__(self, screen, shell: Shell):
+    def __init__(self, screen, shell: ShellProcessIO):
         super().__init__(
-            screen, screen.height, screen.width, has_border=True, name="GrokLog"
+            screen,
+            screen.height,
+            screen.width,
+            has_border=True,
+            name="GrokLog",
+            title="GrokLog",
         )
+
         self.shell = shell
 
-        # Create the widgets for the demo.
-        layout = Layout([100], fill_frame=True)
-        self.add_layout(layout)
-        layout.add_widget(Terminal(name="term", shell=shell, height=25))
-        layout.add_widget(Terminal(name="term", shell=shell, height=25))
-        layout.add_widget(Terminal(name="term", shell=shell, height=25))
-        # layout.add_widget(Terminal(name="term", shell=shell, height=Widget.FILL_FRAME))
+        # Create the Shell layout
+        shell_layout = Layout([100], fill_frame=True)
+        terminal = Terminal(name="term", shell=shell, height=widgets.Widget.FILL_COLUMN)
+        self.add_layout(shell_layout)
+        shell_layout.add_widget(terminal)
+        shell_layout.add_widget(widgets.Divider())
+
+        # Create the Tab Layout and buttons for it
+        add_filter_button = widgets.Button(
+            "Add Filter", lambda: self.display_toast("benis")
+        )
+        tab_layout = Layout([1, 1, 1, 1, 1, 1, 1])
+        self.add_layout(tab_layout)
+        tab_layout.add_widget(add_filter_button, 0)
+        tab_layout.add_widget(widgets.VerticalDivider(), 1)
+
         self.fix()
         self.set_theme("monochrome")
 
