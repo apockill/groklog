566adbbe4818056226fd53c4e1fca8927ce5c919
Alex Thiel <apocthiel@gmail.com>
2021-07-06 13:49:06 -0700

    Move reusable filters to the `conftest.py`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from groklog.process_node import ShellProcessIO
 from tests import utils
 
 
@@ -9,3 +10,20 @@ def every_test_teardown():
     yield
     utils.verify_all_threads_closed()
     utils.verify_all_child_processes_closed()
+
+
+@pytest.fixture()
+def shell():
+    shell = ShellProcessIO()
+    yield shell
+    shell.close(timeout=10)
+
+
+@pytest.fixture()
+def mock_shell():
+    class ShellMock(ShellProcessIO):
+        def __init__(self):
+            # Don't call the parent class here, to avoid starting subprocesses
+            pass
+
+    yield ShellMock()
