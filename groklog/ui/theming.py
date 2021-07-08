f7b9137190b3af21770615538f7f70a228a57a92
Alex Thiel <apocthiel@gmail.com>
2021-07-07 22:51:09 -0700

    Fix contrast of invalid fields + cursor

--- groklog/ui/theming.py
+++ groklog/ui/theming.py
@@ -5,7 +5,7 @@ GROKLOG_THEME = {
     "background": (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK),
     "shadow": (Screen.COLOUR_WHITE, None, Screen.COLOUR_WHITE),
     "disabled": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
-    "invalid": (Screen.COLOUR_YELLOW, Screen.A_BOLD, Screen.COLOUR_YELLOW),
+    "invalid": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_YELLOW),
     "label": (Screen.COLOUR_BLUE, Screen.A_BOLD, Screen.COLOUR_BLACK),
     "borders": (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK),
     "scroll": (Screen.COLOUR_CYAN, Screen.A_NORMAL, Screen.COLOUR_BLACK),
